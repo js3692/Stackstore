356bcb17e17d81c58864db66fc2a37b3d3b540a1
David Kim <Davkime@gmail.com>
2015-11-02 16:46:44 -0500

    added user

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -1,9 +1,14 @@
 'use strict'
+
 var mongoose = require('mongoose');
 var Product = require('./product.js');
 
 var CartSchema = new mongoose.Schema({
-	product: {
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	products: {
 		type: [mongoose.Schema.Types.ObjectId],
 		ref:'Product'
 	}
