2ac5a599bb2fc820de8673bffdf24259f8abbfff
Dan Peach <peach.daniel@gmail.com>
2015-11-02 16:00:36 -0500

    deleted old idea

--- server/main.js
+++ server/main.js
@@ -18,7 +18,7 @@ var startServer = function () {
 
     var PORT = process.env.PORT || 1337;
 
-    server.listen(PORT, function () {
+    server.listen(PORT, function () { 
         console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
     });
 
