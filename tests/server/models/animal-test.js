76ec33b737c9d0214be58ebd279b134fbf38e771
Jungho Son <js3692@users.noreply.github.com>
2015-11-04 12:17:15 -0500

    Wrote some more tests; committing to merge

--- tests/server/models/animal-test.js
+++ tests/server/models/animal-test.js
@@ -3,12 +3,16 @@ var clearDB = require('mocha-mongoose')(dbURI);
 
 var sinon = require('sinon');
 var expect = require('chai').expect;
+var supertest = require('supertest');
+var app = require('../../../server/app');
+var agent = supertest.agent(app);
+
 var mongoose = require('mongoose');
 
 // Require in all models.
 require('../../../server/db/models');
-
 var Animal = mongoose.model('Animal');
+var Review = mongoose.model('Review');
 
 describe('Animal model', function () {
     beforeEach('Establish DB connection', function (done) {
@@ -26,57 +30,49 @@ describe('Animal model', function () {
 
     describe('on creation', function () {
         
-
         it('should require an Animal name', function() {
-            return Animal.create({ description: 'this should fail without an animal name' }).then(null, function(error) {
-                expect(error).to.exist;
-                expect(error.message).to.equal('Animal validation failed');
-            });
+            return Animal.create({ description: 'this should fail without an animal name' })
+                .then(null, function(error) {
+                    expect(error).to.exist;
+                    expect(error.message).to.equal('Animal validation failed');
+                });
+        });
+    });
+
+    describe('reviews property', function () {
+        var animal;
+        beforeEach('create animal', function () {
+            return Animal.create({ animalName: 'TestReviewsAnimal' })
+                .then(function (createdAnimal) {
+                    animal = createdAnimal;
+                });
+        });
+        afterEach('destroy animal', function () {
+            return Animal.remove({ animalName: 'TestReviewsAnimal' });
         });
 
+        it('reviews property should be an array', function() {
+            expect(Array.isArray(animal.reviews)).to.be.true;
+        });
 
-    	describe('reviews property', function () {
-    		var animal;
-
-    		beforeEach('create animal', function () {
-    			return Animal.create({ animalName: 'TestReviewsAnimal'}).then(function (createdAnimal) {
-    				animal = createdAnimal;
-    			})
-
-    		})
-
-    		afterEach('destroy animal', function () {
-    			return Animal.remove({animalName: 'TestReviewsAnimal'})
-    		})
-
-	        it('reviews property should be an array', function() {
-	            expect(Array.isArray(animal.reviews)).to.be.true;
-	        });
-
-			it('the array should contain Review instance ids', function() {
-	            return Review.create({content: 'fake content', stars: 5, dangerLevel: 10}).then(function (newReview) {
-	            	animal.reviews.push(newReview._id);
-	            	expect
-	            })
-
-
-
-	            return Animal.create({ name: "Batman", user: {words: "HELLO"}}).then(null, function(error) {
-	                expect(error).to.exist;
-	                expect(error.message).to.equal('Cart validation failed');
-	            })
-	        });
-        })
-
-
-
-
-
-
-
-
-
-
+        xit('the array should contain Review instance ids', function() {
+            Review.create({
+                content: "I'm a dummy review!",
+                stars: 5,
+                dangerLevel: 3
+            })
+            .then(function (newReview) {
+                agent
+                .post('/api/animals/' + animal._id + '/addReview')
+                .send(newReview)
+                .expect(201)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    expect(res.body.reviews[0].content).to.equal("I'm a dummy review!");
+                    done();
+                });
+            });
+        });
 
     });
 });
