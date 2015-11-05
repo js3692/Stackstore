ba18ebb514513314639637122c4cad35243df4c6
Jungho Son <js3692@users.noreply.github.com>
2015-11-05 11:36:42 -0500

    Added order model

---
+++ server/db/models/order.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	animals: {
+		// Each object will be a snapshot of the
+		// animal document at the time of the purchase.
+		// For example,
+		// {
+		//	_id: "39393393933939339393",
+		//	name: "John the Alaskan snow leopard",
+		//	imageUrl: "www.google.com/billmurray",
+		//	price: 20000,
+		//	description: "Pet me if you can",
+		//	category: ["Dangerous", "Very rare"],
+		//	countryCoude: ['US'],
+		//	conservationStatus: "Endangered",
+		//	rating: 3
+		// }
+		type: [Object],
+		required: true
+	},
+  total: {
+    type: Number,
+    required: true
+  },
+  date: {
+  	type: Date,
+  	required: true
+  },
+  shippingAddr: {
+  	type: String
+  }
+});
+
+schema.methods.deleteOneItem = function(animalId) {
+  
+};
+
+schema.methods.addItem = function(animalId, quantity) {
+ 
+};
+
+mongoose.model('Order', schema);
