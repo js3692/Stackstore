9c623536178b96bbaa02bb0ecfd4d23ac46c2c58
Dan Peach <peach.daniel@gmail.com>
2015-11-05 16:02:44 -0500

    added admin functionality

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,7 +6,7 @@ module.exports = router;
 router.use('/members', require('./members'));
 router.use('/animals', require('./animals'));
 router.use('/cart', require('./cart'));
-
+router.use('/users', require('./users'));
 
 // Make sure this is after all of
 // the registered routes!
