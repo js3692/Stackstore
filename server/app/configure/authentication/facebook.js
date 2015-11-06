69afb87e0ddbdd177805b6f93f3e6725b3aa4090
Anup Bhujle <abhujle7@gmail.com>
2015-11-06 11:06:14 -0500

    added oauth

--- server/app/configure/authentication/facebook.js
+++ server/app/configure/authentication/facebook.js
@@ -9,9 +9,9 @@ module.exports = function (app) {
     var facebookConfig = app.getValue('env').FACEBOOK;
 
     var facebookCredentials = {
-        clientID: facebookConfig.clientID,
-        clientSecret: facebookConfig.clientSecret,
-        callbackURL: facebookConfig.callbackURL
+        clientID: '1006342009407670',
+        clientSecret: '56e75c8da1f3f52b0f350fb523414d8b',
+        callbackURL: 'http://localhost:1337/auth/facebook/callback'
     };
 
     var verifyCallback = function (accessToken, refreshToken, profile, done) {
@@ -22,9 +22,13 @@ module.exports = function (app) {
                 if (user) {
                     return user;
                 } else {
+                    console.log(profile, 'this is the profile')
+                    console.log(profile.displayName);
                     return UserModel.create({
                         facebook: {
-                            id: profile.id
+                            id: profile.id,
+                            username: profile.displayName,
+                            token: accessToken                
                         }
                     });
                 }
