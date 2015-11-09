20494f36bdd753b29486f3d21116cbc05c72c59b
Dan Peach <peach.daniel@gmail.com>
2015-11-09 14:32:23 -0500

    improved add animals page

--- browser/js/addAnimal/addAnimal.controller.js
+++ browser/js/addAnimal/addAnimal.controller.js
@@ -1,5 +1,29 @@
 app.controller('AddAnimalCtrl', function($scope, Animal) {
+    $scope.animal = {};
+    
     $scope.submit = function() {
         Animal.create($scope.animal);
     };
+    
+    $scope.categories = [ 
+                    'Near Threatened', 
+                    'Vulnerable',
+                    'Endangered',
+                    'Critically Endangered',
+                    'Extinct in the Wild',
+                    'Extinct'];
+
+    $scope.status = {
+        isopen: false
+    };
+    
+    $scope.currentStatusOrDefault = function(){
+        if(!$scope.animal.conservationStatus) return "Set Conservation Status";
+        else return $scope.animal.conservationStatus; 
+    };
+    
+    $scope.chooseCategory = function(category){
+        $scope.animal.conservationStatus = category;  
+    };
+
 });
